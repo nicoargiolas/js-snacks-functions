f13dba625fa0b8c514b1b241f5eab5faa2cdf025
nicoargiolas <argiolas.nicola00@gmail.com>
2025-01-09 14:33:30 +0100

    Esercizio 03 completato

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,8 +5,31 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+// function iniziaPer(char, list) {
+//     let listStartWith = [];
+//     for (i = 0; i < list.length; i++) {
+//         if (list[i].charAt(0) === char) {
+//             listStartWith.push(list[i]);
+//         }
+//     }
+//     return listStartWith;
+// }
+
+const iniziaPer = (char, list) => {
+    let listStartWith = [];
+    for (i = 0; i < list.length; i++) {
+        if (list[i].charAt(0) === char) {
+            listStartWith.push(list[i]);
+        }
+    }
+    return listStartWith;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
+let iniziale = prompt("Inserisci una lettera").toUpperCase();
+console.log(iniziaPer(iniziale, names));
+
 
 
 
