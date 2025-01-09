3bd7fd5b305ccc43a390a29db19b82c6cb291d06
nicoargiolas <argiolas.nicola00@gmail.com>
2025-01-09 14:51:22 +0100

    Esercizio 04 completato

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,20 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+// Ho trasformato l'array in una stringa e ho usato un .filter per cercare gli elementi che avevano una vocale con .includes e ho dato in output la lunghezza del nuovo array
+
+// function countVowels(str) {
+//     let numOfVowels = Array.from(str).filter(letter => 'aeiou'.includes(letter)).length;
+
+//     return numOfVowels;
+// }
+
+const countVowels = str => Array.from(str).filter(letter => 'aeiou'.includes(letter)).length;
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
